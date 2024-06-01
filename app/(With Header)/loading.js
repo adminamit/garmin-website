267b25fbfe1088bd3289c024009a36cd37d1be28
Bhuvan Singh <bhuvansingh@192.168.0.200>
2024-06-01 13:18:40 +0700

    Fixed suspense issue causing build error

--- app/(With Header)/loading.js
+++ app/(With Header)/loading.js
@@ -3,7 +3,7 @@ import { Loader } from "@/app/_components/Loader";
 const loader = () => {
     return (
         <div className="my-24">
-            <Loader />
+            {/* <Loader /> */}
             {/*  <h2>Loading</h2> */}
         </div>
     );
