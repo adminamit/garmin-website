baa0e4a4999fccfef4fb45bbd6dd9cd8ba799b11
Bhuvan Singh <bhuvansingh@192.168.0.200>
2024-06-03 21:40:58 +0700

    Patch for  map in blog category page

--- app/_components/Blog/PostList.js
+++ app/_components/Blog/PostList.js
@@ -5,9 +5,10 @@ const PostList = ({ posts }) => {
     return (
         <div className="post-list">
             <div className="post-list__wrapper">
-                {posts.map((post) => {
-                    return <Post post={post} key={post.slug} />;
-                })}
+                {posts &&
+                    posts.map((post) => {
+                        return <Post post={post} key={post.slug} />;
+                    })}
             </div>
             <Pagination />
         </div>
