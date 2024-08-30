5f0f8dbb0421dc17bab14926cb746de5351670b4
adminamit <bhuvan.s@cyberworx.in>
2024-08-30 23:39:24 +0700

    Promo bar api fix

--- app/api/graphQl/settings/route.js
+++ app/api/graphQl/settings/route.js
@@ -1,27 +1,23 @@
 import { NextResponse } from "next/server";
-import { cookies } from "next/headers";
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 export async function GET(request, context) {
-    const allCookies = cookies();
-    const payloadToken = allCookies.get("payload-token").value;
     const res = await fetch(
         process.env.GRAPHQL_API_URL,
         {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                Authorization: `Bearer ${payloadToken}`,
             },
             body: JSON.stringify({
                 query: `
                     {
-                    Settings {
-                        sitePromo
-                        updatedAt
-                        createdAt
-                    }
+                Settings {
+                    sitePromo
+                    updatedAt
+                    createdAt
                 }
+            }
             `,
             }),
         },
